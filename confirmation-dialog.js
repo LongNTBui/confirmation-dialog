5945213033db5e32120f22d547e02c31b2f93871
Long Bui <buingocthanglong@yahoo.com>
2021-01-17 13:24:01 +1100

    Show dialog on click

--- confirmation-dialog.js
+++ confirmation-dialog.js
@@ -1,15 +1,21 @@
-const confirmationDialog = document.createElement('div');
-confirmationDialog.className = 'confirmationHiddenAtStartClass';
-confirmationDialog.id = 'confirmation-dialog';
+let confirmationDialog = document.createElement("div");
+confirmationDialog.className = "confirmationHiddenAtStartClass";
+confirmationDialog.id = "confirmation-dialog";
 
 document.body.appendChild(confirmationDialog);
 // create the dialog and add to body
-document.getElementById('confirmation-dialog').innerHTML =
-    '<div class="confirmation-dialog">' +
-        '<div class="confirmation-content"></div>' +
-        '<div class="confirmation-controls">' +
+document.querySelector("#confirmation-dialog").innerHTML =
+  '<div class="confirmation-dialog">' +
+  '<div class="confirmation-content"></div>' +
+  '<div class="confirmation-controls">' +
+  '<button id="yesBtn" class="yesBtn">Yes</button>' +
+  '<button id="cancelBtn" class="cancelBtn">Cancel</button>' +
+  "</div>" +
+  "</div>";
 
-        '</div>' +
-    '</div>';
-
-
+function showDialog(msg) {
+  document.querySelector(".confirmationHiddenAtStartClass").style.display =
+    "flex";
+  document.querySelector(".confirmation-content").innerHTML = msg;
+  document.querySelector("#confirmation-dialog").focus();
+}
